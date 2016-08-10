f6b78091914df16a545a89c2217c5e95c8c51b27
Andranik Chorokhyan <Andranik_Chorokhyan@epam.com>
2016-08-10 18:47:44 +0400

    Fix bugs

--- TestNGDemo/src/test/java/test/TestClass1.java
+++ TestNGDemo/src/test/java/test/TestClass1.java
@@ -11,18 +11,10 @@ import config.TestConfiguration;
  *
  */
 public class TestClass1 extends TestConfiguration {
-	private int param1;
-	private int param2;
-	@Factory(dataProvider = "staticDataProvider", dataProviderClass = DataProviderClass.class)
-	public TestClass1(int param1, int param2){
-		this.param1 = param1;
-		this.param2 = param2;
-	}
-	
-	
+
 	@Test
 	public void testMethod_1(){
-		System.out.println("Test Class 1 - Test Method 1 - Parameters = " + param1 + ", " + param2);
+		System.out.println("Test Class 1 - Test Method 1 - Parameters = ");
 	}
 	
 	@Test
